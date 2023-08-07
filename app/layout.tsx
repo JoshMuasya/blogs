6da7498cdcbf915ec27374b22ee4dd90063acae6
JoshMuasya <muasyajoshua07@gmail.com>
2023-08-07 12:31:29 +0300

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,26 @@
+import './globals.css';
+import type { Metadata } from 'next';
+
+import Navbar from './components/Navbar';
+import MyProfilePic from './components/MyProfilePic';
+
+export const metadata: Metadata = {
+  title: 'Blog',
+  description: 'Blog Sample with TypeScript',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className="dark:bg-slate-800">
+        <Navbar />
+        <MyProfilePic />
+        {children}
+      </body>
+    </html>
+  )
+}
